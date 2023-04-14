ef7bead85322523eea8916efa570b3769e2b2227
Samuell0003 <2003samuellucas@gmail.com>
2023-04-13 22:14:28 -0300

    created full test

--- src/test/FuncionarioTerceirizadoTeste.java
+++ src/test/FuncionarioTerceirizadoTeste.java
@@ -1,7 +1,42 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
 
+import Funcionario.FuncionarioTerceirizado;
+
 public class FuncionarioTerceirizadoTeste {
-    
+    private FuncionarioTerceirizado funcionarioTerceirizado;
+
+    @Test
+    public void testConstrutorValido() {
+        funcionarioTerceirizado = new FuncionarioTerceirizado("Samuel", 30, 90, 700);
+
+        assertEquals("Samuel", funcionarioTerceirizado.getNome());
+        assertEquals(30, funcionarioTerceirizado.getHotasTrabalhadas());
+        assertEquals(90, funcionarioTerceirizado.getValorHora(), 0);
+        assertEquals( 700, funcionarioTerceirizado.getDespesasAdicionais(), 0);
+    }
+
+    @Test
+    public void testConstrutorInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> { 
+            funcionarioTerceirizado = new FuncionarioTerceirizado("Samuel", 40, 70, 1800);
+        });
+    }
+
+    @Test
+    public void testDespesasAdicionaisValido() {
+        funcionarioTerceirizado = new FuncionarioTerceirizado();
+        funcionarioTerceirizado.setDespesasAdicionais(200);
+    }
+    @Test
+    public void testDespesasAdicionaisInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> { 
+            funcionarioTerceirizado = new FuncionarioTerceirizado();
+            funcionarioTerceirizado.setDespesasAdicionais(3000);
+        });
+    }
 }
--- src/test/FuncionarioTeste.java
+++ src/test/FuncionarioTeste.java
@@ -1,6 +1,87 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 import org.junit.Test;
 
+import Funcionario.Funcionario;
+
 public class FuncionarioTeste {
+    private Funcionario funcionario;
+
+    @Test
+    public void testContrutorValido() {
+        funcionario = new Funcionario("Samuel", 40, 60);
+
+        assertEquals("Samuel", funcionario.getNome());
+        assertEquals(40, funcionario.getHotasTrabalhadas());
+        assertEquals(60, funcionario.getValorHora(), 0);
+
+    }
+
+    @Test
+    public void testConstrutorInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario = new Funcionario("Samuel", 45, 120);
+        });
+    }
+
+    @Test
+    public void testValorHoraValido() {
+        funcionario = new Funcionario();
+        funcionario.setValorHora(80);
+        assertEquals(80, funcionario.getValorHora(), 0);
+    }
+
+    @Test
+    public void testValorHoraValidoInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario = new Funcionario();
+            funcionario.setValorHora(320);
+            // funcionario.getValorHora();
+        });
+    }
+
+    @Test
+    public void testCalcularPagamentoValido() {
+        funcionario = new Funcionario("Samuel", 40, 60);
+
+        assertEquals(2400, funcionario.calcularPagamento(), 0);
+    }
+
+    @Test
+    public void testCalcularPagamentoInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario = new Funcionario("Samuel", 10, 60);
+
+            funcionario.calcularPagamento();
+        });
+    }
+
+    @Test
+    public void testHorasTrabalhadasValido() {
+        funcionario = new Funcionario();
+
+        funcionario.setHotasTrabalhadas(20);
+
+        assertEquals(20, funcionario.getHotasTrabalhadas());
+         
+    }
+
+    @Test
+    public void testHorasTrabalhadasInvalido() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            funcionario = new Funcionario(); 
+            funcionario.setHotasTrabalhadas(70);
+        });
+    }
+
+
+
+
+
+
+    
+
 }
